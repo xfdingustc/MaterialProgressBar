65afe2de784c1e5d023b4d43622d1a50adf4cff3
xfdingustc <xfdingyy@qq.com>
2015-10-21 16:11:29 +0800

    add jcenter gradle script

--- materialprogressbar/src/main/java/com/xfdingustc/materialprogressbar/progressarc/animations/ShrinkArcAnimation.java
+++ materialprogressbar/src/main/java/com/xfdingustc/materialprogressbar/progressarc/animations/ShrinkArcAnimation.java
@@ -8,20 +8,20 @@ import android.view.animation.DecelerateInterpolator;
  * Created by Xiaofei on 2015/7/23.
  */
 public class ShrinkArcAnimation implements ArcAnimation {
-  private ValueAnimator mAnimation;
+    private ValueAnimator mAnimation;
 
-  public ShrinkArcAnimation(ValueAnimator.AnimatorUpdateListener updateListener, Animator.AnimatorListener
-      listener) {
-    mAnimation = ValueAnimator.ofFloat(ArcAnimationFactory.MAX_SWEEP_ANGLE, ArcAnimationFactory
-        .MIN_SWEEP_ANGLE);
-    mAnimation.setInterpolator(new DecelerateInterpolator());
-    mAnimation.setDuration(ArcAnimationFactory.SWEEP_ANIMATOR_DURATION);
-    mAnimation.addUpdateListener(updateListener);
-    mAnimation.addListener(listener);
-  }
+    public ShrinkArcAnimation(ValueAnimator.AnimatorUpdateListener updateListener, Animator.AnimatorListener
+        listener) {
+        mAnimation = ValueAnimator.ofFloat(ArcAnimationFactory.MAX_SWEEP_ANGLE, ArcAnimationFactory
+            .MIN_SWEEP_ANGLE);
+        mAnimation.setInterpolator(new DecelerateInterpolator());
+        mAnimation.setDuration(ArcAnimationFactory.SWEEP_ANIMATOR_DURATION);
+        mAnimation.addUpdateListener(updateListener);
+        mAnimation.addListener(listener);
+    }
 
-  @Override
-  public ValueAnimator getAnimator() {
-    return mAnimation;
-  }
+    @Override
+    public ValueAnimator getAnimator() {
+        return mAnimation;
+    }
 }
