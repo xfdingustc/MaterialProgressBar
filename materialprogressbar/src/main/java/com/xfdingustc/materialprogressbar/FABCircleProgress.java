831a14d10f5669d30777b052f158708f2fc0a957
xfdingustc <xfdingyy@qq.com>
2015-10-23 14:03:09 +0800

    fix typo

--- materialprogressbar/src/main/java/com/xfdingustc/materialprogressbar/FABCircleProgress.java
+++ materialprogressbar/src/main/java/com/xfdingustc/materialprogressbar/FABCircleProgress.java
@@ -20,107 +20,107 @@ import com.xfdingustc.materialprogressbar.progressarc.ProgressArcView;
  */
 @DefaultBehavior(FABCircleProgress.Behavior.class)
 public class FABCircleProgress extends FrameLayout {
-  private final static String TAG = FABCircleProgress.class.getSimpleName();
-  private boolean mViewsAdded = false;
-  private ProgressArcView mProgressArc;
+    private final static String TAG = FABCircleProgress.class.getSimpleName();
+    private boolean mViewsAdded = false;
+    private ProgressArcView mProgressArc;
 
-  private int mArcColor;
-  private int mArcWidth;
-  private boolean mRoundedStroke;
+    private int mArcColor;
+    private int mArcWidth;
+    private boolean mRoundedStroke;
 
-  public FABCircleProgress(Context context) {
-    super(context);
-  }
-
-  public FABCircleProgress(Context context, AttributeSet attrs) {
-    super(context, attrs);
-    init(attrs);
-  }
-
-  public FABCircleProgress(Context context, AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-    init(attrs);
-  }
-
-  @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-  public FABCircleProgress(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-    super(context, attrs, defStyleAttr, defStyleRes);
-    init(attrs);
-  }
-
-  private void init(AttributeSet attrs) {
-    setupInitialAttributes(attrs);
-  }
-
-  private void setupInitialAttributes(AttributeSet attrs) {
-    if (attrs != null) {
-      TypedArray attrArray = getContext().obtainStyledAttributes(attrs, R.styleable
-          .FABCircleProgress, 0, 0);
-
-      mArcColor = attrArray.getColor(R.styleable.FABCircleProgress_arcColor,
-          getResources().getColor(R.color.fab_orange_dark));
-      mArcWidth = attrArray.getDimensionPixelSize(R.styleable.FABCircleProgress_arcWidth,
-          getResources().getDimensionPixelSize(R.dimen.progress_arc_stroke_width));
-      mRoundedStroke = attrArray.getBoolean(R.styleable.FABCircleProgress_roundedStroke, false);
-
-
-      attrArray.recycle();
+    public FABCircleProgress(Context context) {
+        super(context);
     }
-  }
 
-
-  @Override
-  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-    // TODO: we need user fab's size to calculate self size
-    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    if (!mViewsAdded) {
-      addArcView();
-      mViewsAdded = true;
+    public FABCircleProgress(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
     }
-  }
 
-  private void addArcView() {
-    setClipChildren(false);
-    mProgressArc = new ProgressArcView(getContext(), mArcColor, mArcWidth, mRoundedStroke);
+    public FABCircleProgress(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
 
-    LayoutParams params = new FrameLayout.LayoutParams(getFabDimension() + mArcWidth,
-        getFabDimension() + mArcWidth,
-        Gravity.CENTER);
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public FABCircleProgress(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
 
-    addView(mProgressArc, params);
-  }
+    private void init(AttributeSet attrs) {
+        setupInitialAttributes(attrs);
+    }
+
+    private void setupInitialAttributes(AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray attrArray = getContext().obtainStyledAttributes(attrs, R.styleable
+                .FABCircleProgress, 0, 0);
+
+            mArcColor = attrArray.getColor(R.styleable.FABCircleProgress_mpb_arc_color,
+                getResources().getColor(R.color.fab_orange_dark));
+            mArcWidth = attrArray.getDimensionPixelSize(R.styleable.FABCircleProgress_mpb_arc_width,
+                getResources().getDimensionPixelSize(R.dimen.progress_arc_stroke_width));
+            mRoundedStroke = attrArray.getBoolean(R.styleable.FABCircleProgress_mpb_rounded_stroke, false);
 
 
-  // TODO: we need refine this logic;
-  private int getFabDimension() {
-    return getResources().getDimensionPixelSize(R.dimen.fab_size_normal);
+            attrArray.recycle();
+        }
+    }
 
-  }
 
-  public static class Behavior extends CoordinatorLayout.Behavior<FABCircleProgress> {
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // TODO: we need user fab's size to calculate self size
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        if (!mViewsAdded) {
+            addArcView();
+            mViewsAdded = true;
+        }
+    }
 
-    public Behavior() {
+    private void addArcView() {
+        setClipChildren(false);
+        mProgressArc = new ProgressArcView(getContext(), mArcColor, mArcWidth, mRoundedStroke);
+
+        LayoutParams params = new FrameLayout.LayoutParams(getFabDimension() + mArcWidth,
+            getFabDimension() + mArcWidth,
+            Gravity.CENTER);
+
+        addView(mProgressArc, params);
+    }
+
+
+    // TODO: we need refine this logic;
+    private int getFabDimension() {
+        return getResources().getDimensionPixelSize(R.dimen.fab_size_normal);
 
     }
 
-    public boolean layoutDependsOn(CoordinatorLayout parent, FABCircleProgress child, View dependency) {
-      return dependency instanceof Snackbar.SnackbarLayout;
+    public static class Behavior extends CoordinatorLayout.Behavior<FABCircleProgress> {
+
+        public Behavior() {
+
+        }
+
+        public boolean layoutDependsOn(CoordinatorLayout parent, FABCircleProgress child, View dependency) {
+            return dependency instanceof Snackbar.SnackbarLayout;
+        }
+
+        public boolean onDependentViewChanged(CoordinatorLayout parent, FABCircleProgress child, View
+            dependency) {
+            if (dependency instanceof FloatingActionButton) {
+                FloatingActionButton fab = (FloatingActionButton) dependency;
+                child.setScaleX(fab.getScaleX());
+                child.setScaleY(fab.getScaleY());
+            }
+
+            return false;
+        }
+
     }
 
-    public boolean onDependentViewChanged(CoordinatorLayout parent, FABCircleProgress child, View
-        dependency) {
-      if (dependency instanceof FloatingActionButton) {
-        FloatingActionButton fab = (FloatingActionButton) dependency;
-        child.setScaleX(fab.getScaleX());
-        child.setScaleY(fab.getScaleY());
-      }
-
-      return false;
+    public void show() {
+        mProgressArc.show();
     }
-
-  }
-
-  public void show() {
-    mProgressArc.show();
-  }
 }
